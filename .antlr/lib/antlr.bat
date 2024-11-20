2684b91b5d55e9a518659e069dc4e01e6bf0fcf6
Harry Huynh <phuongdai.saigon@gmail.com>
2024-11-20 13:05:35 +0700

    test

--- .antlr/lib/antlr.bat
+++ .antlr/lib/antlr.bat
@@ -1,1 +1,1 @@
-java -cp .;.\lib\antlr-4.0-complete.jar;%CLASSPATH% org.antlr.v4.Tool %* Statements.g4 -no-listener -visitor
+java org.antlr.v4.Tool %*
--- .antlr/lib/grun.bat
+++ .antlr/lib/grun.bat
@@ -1,1 +1,1 @@
-java -cp .;.\lib\antlr-4.0-complete.jar;%CLASSPATH% org.antlr.v4.runtime.misc.TestRig Statements prog -gui %1
+java org.antlr.v4.gui.TestRig %*
